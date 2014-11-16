f0f65ca8732a45b7b750418ee49c7a881b842c82
Raj Bharath <rbrajbharath1@gmail.com>
2014-11-17 00:49:16 +0530

    Initial Draft - My first Spring Application using Spring Boot

---
+++ src/main/java/hello/HelloController.java
@@ -0,0 +1,17 @@
+package hello;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+    @RequestMapping("/")
+    public String index() {
+        return "Greetings from Spring boot";
+    }
+
+    @RequestMapping("/hi")
+    public String sayHi() {
+        return "Hi";
+    }
+}
